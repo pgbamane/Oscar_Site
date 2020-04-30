11fc0bfd51b92a072b07f9024fc00ea592bcc250
pgbamane <pradnya.bamane23@gmail.com>
2020-04-30 20:08:47 +0530

    media url added

--- Oscar_Site/urls.py
+++ Oscar_Site/urls.py
@@ -2,13 +2,17 @@ from django.conf.urls import url
 from oscar.app import application
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
-urlpatterns = [
-    # The Django admin is not officially supported; expect breakage.
-    # Nonetheless, it's often useful for debugging.
-    path('admin/', admin.site.urls),
+urlpatterns = \
+    [
+        # The Django admin is not officially supported; expect breakage.
+        # Nonetheless, it's often useful for debugging.
+        path('admin/', admin.site.urls),
 
-    path('i18n/', include('django.conf.urls.i18n')),
+        path('i18n/', include('django.conf.urls.i18n')),
 
-    path('', application.urls),
-]
+        # oscar urls
+        path('', application.urls),
+    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
