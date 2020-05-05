ab5b7fcce1d939ea0a6507247878619070934044
pgbamane <pradnya.bamane23@gmail.com>
2020-05-05 12:54:12 +0530

    success_url changed to account_login

--- apps/user/views.py
+++ apps/user/views.py
@@ -15,7 +15,7 @@ class SignupView(AllAuthSignupView):
     template_name = "account/signup.html"
     form_class = SignupForm
     redirect_field_name = None
-    success_url = reverse_lazy('home')
+    success_url = reverse_lazy('account_login')
 
     # def get_form_class(self):
     #     return get_form_class(settings.FORMS, 'signup', self.form_class)
