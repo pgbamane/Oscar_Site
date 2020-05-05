e0e8f63b464c91da118d405e814cb22bf8c4fe96
pgbamane <pradnya.bamane23@gmail.com>
2020-05-05 12:08:18 +0530

    custom urls for account_signup, account_login, account_logout, account_inactive, social_account_signup, accounts added

--- Oscar_Site/urls.py
+++ Oscar_Site/urls.py
@@ -4,6 +4,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from allauth.account.views import LoginView, AccountInactiveView, LogoutView
+from apps.user import views
+# from allauth.socialaccount.views import S
+from allauth.socialaccount.providers.oauth2.urls import default_urlpatterns
+from allauth.socialaccount.providers.google.provider import GoogleProvider
+from allauth.socialaccount.views import SignupView
 
 urlpatterns = \
     [
@@ -17,12 +23,12 @@ urlpatterns = \
         path('', application.urls),
 
         # all-auth urls
-        # path('accounts/signup/', views.SignupView.as_view(), name="account_signup"),
-        # path('accounts/login/', LoginView.as_view(), name="account_login"),
-        # path('accounts/logout/', LogoutView.as_view(), name="account_logout"),
-        # path('accounts/inactive', AccountInactiveView.as_view(), name="account_inactive"),
-        # path('accounts/socialaccount_signup', SignupView.as_view(), name='socialaccount_signup'),
+        path('accounts/signup/', views.SignupView.as_view(), name="account_signup"),
+        path('accounts/login/', LoginView.as_view(), name="account_login"),
+        path('accounts/logout/', LogoutView.as_view(), name="account_logout"),
+        path('accounts/inactive', AccountInactiveView.as_view(), name="account_inactive"),
+        path('accounts/socialaccount_signup', SignupView.as_view(), name='socialaccount_signup'),
         # # path('accounts/socialaccount_signup', SignupView.as_view(), name='socialaccount_signup'),
-        # path('accounts/', include(default_urlpatterns(GoogleProvider))),
-        path('accounts/', include('allauth.urls')),
+        path('accounts/', include(default_urlpatterns(GoogleProvider))),
+        # path('accounts/', include('allauth.urls')),
     ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
