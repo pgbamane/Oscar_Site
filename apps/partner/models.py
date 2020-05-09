de822cd33895293304176076a558bac14364d233
pgbamane <pradnya.bamane23@gmail.com>
2020-05-10 00:35:20 +0530

    in stockrecord model pricing_strategy field added

--- apps/partner/models.py
+++ apps/partner/models.py
@@ -1,1 +1,21 @@
+from oscar.apps.partner.abstract_models import AbstractStockRecord
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+PRICING_OPTIONS = (
+    ('PREMIUM', 'Premium'),
+    ('GOLD', 'Gold'),
+    ('', 'None'),
+)
+
+
+class StockRecord(AbstractStockRecord):
+    pricing_strategy = models.CharField(max_length=100,
+                                        default=PRICING_OPTIONS[2][0],
+                                        choices=PRICING_OPTIONS,
+                                        blank=True,
+                                        help_text=_(
+                                            "Specify Premium or Gold for child products. Not nessasary for Parent"))
+
+
 from oscar.apps.partner.models import *  # noqa isort:skip
