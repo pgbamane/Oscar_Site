4efdecb2bbd14d9d8dda5f961d6e6068a2e4dfb7
pgbamane <pradnya.bamane23@gmail.com>
2020-05-09 23:50:37 +0530

    Product model unit_price, quantity_per_unit, total_price, is_active removed and quantity and quantity_unit renamed as weight and weight_unit

--- apps/catalogue/models.py
+++ apps/catalogue/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from oscar.apps.catalogue.abstract_models import AbstractProduct
 
-QAUNTITY_UNIT_CHOICES = (
+WEIGHT_UNIT_CHOICES = (
     ('kilogram', 'Kg'),
     ('litre', 'Ltr'),
     ('millilitre', 'ML')
@@ -21,26 +21,26 @@ class Product(AbstractProduct):
     # id = models.AutoField(primary_key=True, )
     # product_name = models.CharField(max_length=50, default='')
 
-    quantity_per_unit = models.IntegerField(blank=True,
-                                            null=True,
-                                            help_text=_(
-                                                    'quantity that items are shipped per unit. Ex. 1Ltr per bottle of oil'))
-    unit_price = models.FloatField(blank=True,
-                                   null=True,
-                                   help_text=_('Price per unit of product. Ex. Price of 1Ltr oil per bottle'))
+    # quantity_per_unit = models.IntegerField(blank=True,
+    #                                         null=True,
+    #                                         help_text=_(
+    #                                                 'quantity that items are shipped per unit. Ex. 1Ltr per bottle of oil'))
+    # unit_price = models.FloatField(blank=True,
+    #                                null=True,
+    #                                help_text=_('Price per unit of product. Ex. Price of 1Ltr oil per bottle'))
 
-    total_quantity = models.IntegerField(help_text=_('Total weight of this Product Purchase'), null=True, blank=True)
-    total_price = models.FloatField(help_text=_('Total price of this Product Purchase'), null=True, blank=True)
+    weight_unit = models.CharField(max_length=20,
+                                   choices=WEIGHT_UNIT_CHOICES,
+                                   default='',
+                                   help_text=_(
+                                       'Unit used to measure Weight. Ex. Kilogram used to measure quanitity of Pulses. '
+                                       'Optional for child product. For parent specify it.'))
 
-    quantity_unit = models.CharField(max_length=20,
-                                     choices=QAUNTITY_UNIT_CHOICES,
-                                     default='',
-                                     help_text=_(
-                                         'Unit used to measure Quantity. Ex. Kilogram used to measure quanitity of Pulses. '
-                                         'Optional for child product. For parent specify it.'))
+    weight = models.IntegerField(help_text=_('Total weight of this Product Purchase'), null=True, blank=True)
+    # total_price = models.FloatField(help_text=_('Total price of this Product Purchase'), null=True, blank=True)
 
-    is_active = models.BooleanField(default=True,
-                                    help_text=_("Is it ready to sale or not ?."))
+    # is_active = models.BooleanField(default=True,
+    #                                 help_text=_("Is it ready to sale or not ?."))
     pricing_strategy = models.CharField(max_length=100,
                                         default=PRICING_OPTIONS[2][0],
                                         choices=PRICING_OPTIONS,
