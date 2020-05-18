50ffdf9aa94edeb7f918f6170c900e8e83c92e89
pgbamane <pradnya.bamane23@gmail.com>
2020-05-19 04:21:22 +0530

    product_info url added to Catalogue Application

---
+++ apps/catalogue/app.py
@@ -0,0 +1,21 @@
+from django.conf.urls import url
+from oscar.apps.catalogue.app import CatalogueApplication as CoreCatalogueApplication
+# from .views import P
+
+
+from oscar.core.loading import get_class
+
+
+class CatalogueApplication(CoreCatalogueApplication):
+    product_info_view = get_class('catalogue.views', 'ProductInfo')
+
+    def get_urls(self):
+        urlpatterns = super(CatalogueApplication, self).get_urls()
+        urlpatterns += [
+            url(r'^product_info/(?P<product_id>[\d]+)/$', self.product_info_view.as_view(), name='product_info'),
+        ]
+        return self.post_process_urls(urlpatterns)
+        # return urlpatterns
+
+
+application = CatalogueApplication()
