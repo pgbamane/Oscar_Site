242b2c5f7c215d938c0b510c3593e98e292e3abb
pgbamane <pradnya.bamane23@gmail.com>
2020-05-19 07:09:36 +0530

    ProductInfo view extending and using Django DetailView

--- apps/catalogue/views.py
+++ apps/catalogue/views.py
@@ -4,16 +4,18 @@ from django.views.generic import View
 from oscar.core.loading import get_model
 
 
-# def update_product_info(request, *args, product_id):
-#     print("Product Id:", product_id)
-#     return render(request, 'catalogue/partials/product_info.html')
+# class ProductInfo(View):
+#     Product = get_model('catalogue', 'Product')
+#
+#     def get(self, request, *args, **kwargs):
+#         # print("Product Id:", product_id)
+#         product_id = kwargs.get('product_id', None)
+#         product = Product.objects.get(pk=product_id)
+#         # super(ProductInfo, self).get(request, *args, **kwargs)
+#         return render(request, 'catalogue/partials/product_info.html', {'product': product})
 
-class ProductInfo(View):
-    Product = get_model('catalogue', 'Product')
-
-    def get(self, request, *args, **kwargs):
-        # print("Product Id:", product_id)
-        product_id = kwargs.get('product_id', None)
-        product = Product.objects.get(pk=product_id)
-        # super(ProductInfo, self).get(request, *args, **kwargs)
-        return render(request, 'catalogue/partials/product_info.html', {'product': product})
+class ProductInfo(DetailView):
+    model = get_model('catalogue', 'Product')
+    queryset = model.objects.all()
+    template_name = 'catalogue/partials/product_info.html'
+    context_object_name = 'product'
