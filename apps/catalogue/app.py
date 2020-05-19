19b0bb7a4d36269bcc4b628658df1b895e505173
pgbamane <pradnya.bamane23@gmail.com>
2020-05-19 08:36:27 +0530

    stock_record view url added to catalohue

--- apps/catalogue/app.py
+++ apps/catalogue/app.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
 from oscar.apps.catalogue.app import CatalogueApplication as CoreCatalogueApplication
+# from ..partner import views as partner_
 # from .views import P
 
 
@@ -8,11 +9,13 @@ from oscar.core.loading import get_class
 
 class CatalogueApplication(CoreCatalogueApplication):
     product_info_view = get_class('catalogue.views', 'ProductInfo')
+    stock_record_view = get_class('partner.views', 'StockRecord')
 
     def get_urls(self):
         urlpatterns = super(CatalogueApplication, self).get_urls()
         urlpatterns += [
             url(r'^product_info/(?P<pk>[\d]+)/$', self.product_info_view.as_view(), name='product_info'),
+            url(r'^stock_record/(?P<pk>[\d]+)/$', self.stock_record_view.as_view(), name='stock_record'),
         ]
         return self.post_process_urls(urlpatterns)
         # return urlpatterns
