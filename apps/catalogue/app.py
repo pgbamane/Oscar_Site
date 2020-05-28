8211d4df1b8e4912e185e245d8fdae756a9fcf44
pgbamane <pradnya.bamane23@gmail.com>
2020-05-28 09:38:05 +0530

    url for PriceDetails view created and stock_record url removed

--- apps/catalogue/app.py
+++ apps/catalogue/app.py
@@ -9,13 +9,13 @@ from oscar.core.loading import get_class
 
 class CatalogueApplication(CoreCatalogueApplication):
     product_info_view = get_class('catalogue.views', 'ProductInfo')
-    stock_record_view = get_class('partner.views', 'StockRecord')
+    price_details_view = get_class('catalogue.views', 'PriceDetails')
 
     def get_urls(self):
         urlpatterns = super(CatalogueApplication, self).get_urls()
         urlpatterns += [
             url(r'^product_info/(?P<pk>[\d]+)/$', self.product_info_view.as_view(), name='product_info'),
-            url(r'^stock_record/(?P<pk>[\d]+)/$', self.stock_record_view.as_view(), name='stock_record'),
+            url(r'^price_details/(?P<pk>[\d]+)/$', self.price_details_view.as_view(), name='price_details'),
         ]
         return self.post_process_urls(urlpatterns)
         # return urlpatterns
