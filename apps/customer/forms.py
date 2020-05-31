b59bcfee11c5db49d19daf056dfd1f1824ec4f69
pgbamane <pradnya.bamane23@gmail.com>
2020-05-31 13:08:36 +0530

    create ProfileForm extending Oscar's UserForm

--- apps/customer/forms.py
+++ apps/customer/forms.py
@@ -1,13 +1,15 @@
 from crispy_forms.bootstrap import InlineRadios
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Row, Column, Field, Submit, Reset
-from django.forms import EmailField
+# from django.forms import EmailField
+from oscar.apps.customer.forms import UserForm
+from django import forms
 from django.urls import reverse
-from oscar.apps.customer.forms import *
 # from oscar.apps.customer.forms import ProfileForm as UserForm, EmailUserCreationForm as CoreEmailUserCreationForm
 from oscar.core.compat import (
     existing_user_fields, get_user_model
 )
+from oscar.core.loading import get_profile_class
 from allauth.account.forms import SignupForm as CoreSignUpForm, PasswordField
 from django.utils.translation import ugettext_lazy as _
 
@@ -161,13 +163,13 @@ class SignupForm(CoreSignUpForm):
     def __init__(self, *args, **kwargs):
         super(SignupForm, self).__init__(*args, **kwargs)
         self.fields['password2'] = PasswordField(label=_("Confirm Password"))
-        self.fields['email'] = EmailField(label=_('Email ID'),
-                                          widget=forms.EmailInput(
-                                              attrs={
-                                                  'class': 'form-control',
-                                                  'placeholder': 'Email ID'
-                                              }
-                                          ))
+        self.fields['email'] = forms.EmailField(label=_('Email ID'),
+                                                widget=forms.EmailInput(
+                                                    attrs={
+                                                        'class': 'form-control',
+                                                        'placeholder': 'Email ID'
+                                                    }
+                                                ))
 
     def save(self, request):
         print("\nSign Up Form Save Method Called: ")
@@ -178,7 +180,6 @@ class SignupForm(CoreSignUpForm):
     @property
     def helper(self):
         helper = FormHelper()
-
         helper.form_id = 'id-Sign-Up-Form'
         helper.form_method = 'POST'
         helper.form_action = reverse('account_signup')
@@ -219,3 +220,175 @@ class SignupForm(CoreSignUpForm):
         )
         # helper.attrs('novalidate')
         return helper
+
+
+class ProfileForm(UserForm):
+    first_name = forms.CharField(max_length=30,
+                                 required=False,
+                                 label="First Name",
+                                 widget=forms.TextInput(
+                                     attrs={
+                                         'class': 'form-control',
+                                         'placeholder': 'Enter First Name',
+                                         'autofocus': 'autofocus',
+                                     }))
+    last_name = forms.CharField(max_length=30,
+                                required=False,
+                                label="Last Name",
+                                widget=forms.TextInput(
+                                    attrs={
+                                        'class': 'form-control',
+                                        'placeholder': 'Enter Last Name',
+                                        'autofocus': 'autofocus',
+                                    }
+                                ))
+    gender = forms.ChoiceField(label="Gender",
+                               choices=GENDER_OPTIONS,
+                               initial=GENDER_OPTIONS[0][0],
+                               widget=forms.RadioSelect(
+                                   # attrs={
+                                   #     'class': 'form-control',
+                                   # },
+                                   # choices=GENDER_OPTIONS
+                               ))
+    # phone_number = forms.CharField(max_length=13, label="Phone No.")
+
+    address = forms.CharField(max_length=255,
+                              required=False,
+                              # help_text="Flat No, Building, Street, Area",
+                              label="Address (Flat No, Building, Street, Area)",
+                              widget=forms.TextInput(
+                                  attrs={
+                                      'class': 'form-control',
+                                      'placeholder': 'Address',
+                                      'autofocus': 'autofocus',
+                                  }
+                              ))
+    locality = forms.CharField(max_length=50,
+                               required=False,
+                               # help_text='Locality/Town',
+                               label="Locality/Town",
+                               widget=forms.TextInput(
+                                   attrs={
+                                       'class': 'form-control',
+                                       'placeholder': 'Locality',
+                                       'autofocus': 'autofocus',
+                                   }
+                               )
+                               )
+    state = forms.CharField(max_length=50,
+                            required=False,
+                            label="State",
+                            widget=forms.TextInput(
+                                attrs={
+                                    'class': 'form-control',
+                                    'placeholder': 'State',
+                                    'autofocus': 'autofocus',
+                                }
+                            ))
+    district = forms.CharField(max_length=50,
+                               required=False,
+                               label="District",
+                               widget=forms.TextInput(
+                                   attrs={
+                                       'class': 'form-control',
+                                       'placeholder': 'District',
+                                       'autofocus': 'autofocus',
+                                   }
+                               )
+                               )
+    city = forms.CharField(max_length=50,
+                           required=False,
+                           # help_text="City or Taluka",
+                           label="City/Taluka",
+                           widget=forms.TextInput(
+                               attrs={
+                                   'class': 'form-control',
+                                   'placeholder': 'City',
+                                   'autofocus': 'autofocus',
+                               }
+                           ))
+    pincode = forms.CharField(max_length=10,
+                              required=False,
+                              # help_text="Pincode stored as Chars",
+                              label="Pincode",
+                              widget=forms.TextInput(
+                                  attrs={
+                                      'class': 'form-control',
+                                      'placeholder': 'Pincode',
+                                      'autofocus': 'autofocus',
+                                  }
+                              ))
+
+    phone_number = forms.CharField(max_length=13,
+                                   required=False,
+                                   label="Phone No.",
+                                   widget=forms.TextInput(
+                                       attrs={
+                                           'class': 'form-control',
+                                           'placeholder': 'Phone Number',
+                                           'autofocus': 'autofocus',
+                                       }
+                                   ))
+
+    # def __init__(self, user, *args, **kwargs):
+    #     self.user = user
+    #     # kwargs['instance'] = user
+    #     # signup_kwargs = kwargs
+    #     # signup_kwargs.pop('instance')
+    #     super(UserForm, self).__init__(*args, **kwargs)
+
+    class Meta:
+        model = User
+        fields = existing_user_fields(['first_name', 'last_name', 'gender', 'address', 'locality',
+                                       'city', 'state', 'district', 'email',
+                                       'pincode', 'phone_number'])
+
+    @property
+    def helper(self):
+        helper = FormHelper()
+        helper.form_id = 'id-Profile-Update-Form'
+        helper.form_method = 'POST'
+        helper.form_action = reverse('customer:profile-update')
+        # helper.field_class = "col-md-6"
+        # helper.label_class = "col-md-6"
+        # helper.
+        helper.layout = Layout(
+            Row(
+                Column('first_name', css_class="form-group col-sm-5 mb-0"),
+                Column('last_name', css_class='form-group col-sm-5 mb-0'),
+                css_class='form-row'
+            ),
+            InlineRadios('gender', css_class="form-group"),
+            Field('address', css_class="form-group col-md-9 mb-0"),
+            Row(
+                Column('locality', css_class="form-group col-sm-5 mb-0"),
+                Column('state', css_class='form-group col-sm-5 mb-0'),
+                css_class='form-row'
+            ),
+            # Field('locality'),
+            # 'state',
+            Row(
+                Column('district', css_class="form-group col-md-4 mb-0"),
+                Column('city', css_class="form-group col-md-3 mb-0"),
+                Column('pincode', css_class="form-group col-md-3 mb-0"),
+                css_class="form-row"
+            ),
+            Field('phone_number', css_class="form-group col-md-10 mb-0"),
+            Field('email', css_class="form-group col-md-10 mb-0"),
+            # Field('password1', css_class="form-group col-md-10 mb-0"),
+            # Field('password2', css_class="form-group col-md-10 mb-0"),
+            # # Column('city'),
+            Row(
+                Submit('submit', 'Save', css_class="form-group btn col-md-offset-2 col-md-2 mb-0",
+                       css_id='sign-up-id'),
+                Reset('reset', 'Reset', css_class='form-group btn col-md-offset-2 col-md-2 mb-0 btn-danger')
+            )
+        )
+        # helper.attrs('novalidate')
+        return helper
+
+# # UserForm = SignupForm
+# Profile = get_profile_class()
+# if not Profile:
+#     ProfileForm = UserForm
