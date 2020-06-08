3c2173ae650cb9b8e4e3310304a920beb280b9c2
pgbamane <pradnya.bamane23@gmail.com>
2020-06-08 09:52:47 +0530

    add EMAIL_REQUIRED_ERROR

--- apps/customer/validators.py
+++ apps/customer/validators.py
@@ -1,2 +1,3 @@
 BIRTHDAY_INVALID_ERROR = 'Birthday is invalid'
+EMAIL_REQUIRED_ERROR = 'Email is required.'
 EMAIL_INVALID_DOMAIN_ERROR = ['Email address should contain a valid domain(Gmail, Yahoo, Only)']
