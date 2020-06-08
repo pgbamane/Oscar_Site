43b63715dd7f5e1f65c9de5da3d924ff1c215027
pgbamane <pradnya.bamane23@gmail.com>
2020-06-08 12:00:12 +0530

    password not equal error message added

--- apps/customer/validators.py
+++ apps/customer/validators.py
@@ -2,3 +2,4 @@ BIRTHDAY_INVALID_ERROR = 'Birthday is invalid.'
 EMAIL_REQUIRED_ERROR = 'Email is required.'
 EMAIL_INVALID_DOMAIN_ERROR = 'Email address should contain a valid domain(Gmail, Yahoo, Only).'
 EMAIL_ALREADY_TAKEN_ERROR = "A user is already registered with this e-mail address."
+PASSWORD_NOT_SAME_ERROR = "You must type the same password each time."
