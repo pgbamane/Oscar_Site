27da8c375acb7845405871d8f8d3e0333db88624
pgbamane <pradnya.bamane23@gmail.com>
2020-06-08 10:37:15 +0530

    email already taken error added

--- apps/customer/validators.py
+++ apps/customer/validators.py
@@ -1,3 +1,4 @@
-BIRTHDAY_INVALID_ERROR = 'Birthday is invalid'
+BIRTHDAY_INVALID_ERROR = 'Birthday is invalid.'
 EMAIL_REQUIRED_ERROR = 'Email is required.'
-EMAIL_INVALID_DOMAIN_ERROR = ['Email address should contain a valid domain(Gmail, Yahoo, Only)']
+EMAIL_INVALID_DOMAIN_ERROR = 'Email address should contain a valid domain(Gmail, Yahoo, Only).'
+EMAIL_ALREADY_TAKEN_ERROR = "A user is already registered with this e-mail address."
