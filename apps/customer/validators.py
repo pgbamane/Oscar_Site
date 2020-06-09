f5185aa7d6a4f8cf29c0d37f031b6466afd7cc93
pgbamane <pradnya.bamane23@gmail.com>
2020-06-09 22:15:49 +0530

    incorrect date format error message created

--- apps/customer/validators.py
+++ apps/customer/validators.py
@@ -1,4 +1,5 @@
 BIRTHDAY_INVALID_ERROR = 'Birthday is invalid.'
+DATE_INCORRECT_FORMAT_ERROR = 'Incorrect data format, should be DD-MM-YYYY'
 EMAIL_REQUIRED_ERROR = 'Email is required.'
 EMAIL_INVALID_DOMAIN_ERROR = 'Email address should contain a valid domain(Gmail, Yahoo, Only).'
 EMAIL_ALREADY_TAKEN_ERROR = "A user is already registered with this e-mail address."
