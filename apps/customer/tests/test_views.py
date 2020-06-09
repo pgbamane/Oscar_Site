87e42517a93a3c91f434cabc6e30563865d557a9
pgbamane <pradnya.bamane23@gmail.com>
2020-06-10 05:28:31 +0530

    singup view post valid renamed to ajax post and modified to check signed user can login

--- apps/customer/tests/test_views.py
+++ apps/customer/tests/test_views.py
@@ -1,11 +1,14 @@
+import datetime
 from django import test
-from django.contrib.auth import get_user_model
-import json
+from oscar.core.compat import get_user_model
 from django.test import Client, RequestFactory
 from django.urls import reverse
-
-from ..forms import SignupForm, ProfileForm, FEMALE
+from ..forms import SignupForm, ProfileForm, BIRTHDAY_FORMAT
 from ..views import SIGNUP_PAGE_MESSAGE
+from apps.users.models import FEMALE, MALE
+import json
+
+User = get_user_model()
 
 
 class SignupTests(test.TestCase):
@@ -44,42 +47,42 @@ class SignupTests(test.TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.context['form'].initial['gender'], FEMALE)
 
-    def test_post_request_form_valid_data(self):
+    def test_ajax_post_valid_data(self):
         signup_form_data = {
             'first_name': 'Akshay',
             'last_name': 'Satpute',
-            'gender': 'male',
+            'gender': MALE,
+            'birthday': datetime.date(2000, 1, 30).strftime(BIRTHDAY_FORMAT),
             'phone_number': '7878457845',
             'email': 'akshay@gmail.com',
             'password1': 'satputeps',
             'password2': 'satputeps'
         }
         form = SignupForm(signup_form_data)
-        print("\n\nForm Valid:", form.is_valid())
+        self.assertTrue(form.is_valid())
         response = self.client.post(reverse('account_signup'),  # by default sends data in 'multipart/form-data; '
-                                    data=signup_form_data,  # json data
+                                    data=signup_form_data,
+                                    # json data
                                     # content_type='application/json',
                                     # for ajax request
                                     HTTP_X_REQUESTED_WITH='XMLHttpRequest')
 
-        self.assertEqual(response['content-type'], 'application/json')
-        print("\nResponse Status Code:", response.status_code)
         self.assertEqual(response.status_code, 200)
-        print("\nResponse Content(String representation) :", response.content)
-        # self.assertFormError(response, 'form', None, [])
+        self.assertEqual(response['content-type'], 'application/json')
+        # print("\nResponse Content(String representation) :", response.content)
+        # data = response.json() both data gettting ways are same
+        data = json.loads(response.content.decode('utf8'))
 
         print("\n Response Json: ", response.json())
-        self.assertTrue(response.json())
-        print("\n Response Json Location:", response.json()['location'])
-        self.assertEqual(response.json()['location'], reverse('account_login'))
-
-        print("\n Response Json Form:", response.json()['form'])
-        self.assertTrue(response.json()['form'])
-        print("\n Response HTML Form:", response.json()['html'])
-        self.assertFalse(response.json()['html'])
-        self.assertTrue(get_user_model().objects.get(email='akshay@gmail.com'))
-        # self.assertEqual(request.)
-        # form.signup(customer_final=get_user_model())
+        self.assertTrue(data)
+        self.assertIn('location', data)
+        self.assertEqual(data['location'], reverse('account_login'))
+        self.assertIn('form', data)
+        print("\n Response HTML Form:", data['html'])
+        self.assertFalse(data['html'])
+        user = User.objects.get(email=form.cleaned_data['email'])
+        #     check whether this user can login
+        self.client.login(email=user.email, password=user.password)
 
     def test_post_request_form_invalid_email(self):
         signup_form_data = {
