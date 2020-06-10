352b180f50595d34263ec6b65b7d27e3a21ecf96
pgbamane <pradnya.bamane23@gmail.com>
2020-06-10 12:18:05 +0530

    create Name's required and password's required error messages

--- apps/customer/validators.py
+++ apps/customer/validators.py
@@ -2,12 +2,16 @@ import datetime
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
 
+FIRST_NAME_REQUIRED_ERROR = 'First Name is required.'
+LAST_NAME_REQUIRED_ERROR = 'Last Name is required.'
 BIRTHDAY_INVALID_ERROR = 'Birthday is invalid.'
 DATE_INCORRECT_FORMAT_ERROR = 'Incorrect date format, should be DD-MM-YYYY'
 EMAIL_REQUIRED_ERROR = 'Email is required.'
 EMAIL_INVALID_ERROR = 'Enter a valid email address.'
 EMAIL_INVALID_DOMAIN_ERROR = 'Email address should contain a valid domain(Gmail, Yahoo, Only).'
 EMAIL_ALREADY_TAKEN_ERROR = "A user is already registered with this e-mail address."
+PASSWORD1_REQUIRED_ERROR = 'Password is required.'
+PASSWORD2_REQUIRED_ERROR = 'Confirm Password is required.'
 PASSWORD_NOT_SAME_ERROR = "You must type the same password each time."
 
 DATE_FORMAT = "%d-%m-%Y"
